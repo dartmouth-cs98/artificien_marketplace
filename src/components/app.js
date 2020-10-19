cd302f4694d975f0cb0bc69d9ab5ad1346a638f4
Alex Quill <alexquill1@gmail.com>
2020-10-19 16:12:14 -0400

    database hello world

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,7 @@ const Welcome = (props) => {
   return (
     <div>
       Welcome
+      <Button>Tobias is a Weeb</Button>
       <Counter />
       <Controls />
     </div>
@@ -24,7 +25,7 @@ const About = (props) => {
   return <div> All there is to know about me </div>;
 };
 const Test = (props) => {
-  return <div> ID: { props.match.params.id } </div>;
+  return <div> ID: {props.match.params.id} </div>;
 };
 const FallBack = (props) => {
   return <div> URL Not Found </div>;
@@ -71,3 +72,115 @@ const Nav = (props) => {
 export default App;
 
 // ReactDOM.render(<App />, document.getElementById('main'));
+
+// -------------------------- Imports -----------------------------
+var AWS = require('aws-sdk');
+
+const path = require('path')
+const dirPath = path.join(__dirname, '../../config.JSON')
+AWS.config.loadFromPath(dirPath);
+
+var DOC = require("dynamodb-doc");
+docClient = new DOC.DynamoDB();
+
+
+// -------------------------- SCAN Params -----------------------------
+
+
+var scanParams = {
+  TableName: "hello_world_table" // give it your table name 
+};
+
+// -------------------------- CRUD Params -----------------------------
+
+// var getParams = {
+//   Key: {
+//     "user_id": "kenneym"
+//   },
+//   TableName: "hello_world_table"
+// };
+
+// var putParams = {
+//   Item: {
+//     "user_id": "bigChungus",
+//     "Name": "Tobias Weebington",
+//     "Skills": {
+//       "Swag": 50,
+//       "Genius": "No"
+//     }
+//   },
+//   ReturnConsumedCapacity: "TOTAL",
+//   TableName: "hello_world_table"
+// };
+
+// var upParams = {
+//   Key: {
+//     "user_id": "bigChungus",
+//   },
+//   AttributeUpdates: {
+//     'Name': {
+//       Action: "PUT",
+//       Value: "JEFFERY EPSTEIN"
+//     }
+//   },
+//   ReturnValues: "ALL_OLD",
+//   ReturnValues: "ALL_NEW",
+//   TableName: "hello_world_table"
+// };
+
+// var delParams = {
+//   Key: {
+//     "user_id": "bigChungus"
+//   },
+//   ReturnValues: "ALL_OLD",
+//   TableName: "hello_world_table"
+// }
+
+// -------------------------- DB FUNCTIONS -----------------------------
+
+
+docClient.scan(scanParams, function (err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    console.log("\n--------SCAN--------");
+    console.log(JSON.stringify(data, null, 2));
+  }
+});
+
+// docClient.getItem(getParams, function (err, data) {
+//     if (err) {
+//         console.log(err, err.stack);
+//     } else {
+//         console.log("--------GET-------")
+//         console.log(JSON.stringify(data.Item, null, 2));
+//         // document.getElementById("dbinfo").innerHTML = "big chungus"
+//     }
+// });
+
+// docClient.putItem(putParams, function (err, data) {
+//     if (err) {
+//         console.log(err, err.stack);
+//     } else {
+//         console.log("--------PUT-------")
+//         console.log(JSON.stringify(data, null, 2));
+//     }
+// });
+
+// docClient.updateItem(upParams, function (err, data) {
+//     if (err) {
+//         console.log(err, err.stack);
+//     } else {
+//         console.log("--------UPDATE-------")
+//         console.log(JSON.stringify(data, null, 2));
+//     }
+// });
+
+// docClient.deleteItem(delParams, function (err, data) {
+//     if (err) {
+//         console.log(err, err.stack);
+//     } else {
+//         console.log("--------DEL-------")
+//         console.log(JSON.stringify(data, null, 2));
+//     }
+// });
