a80d14433d8ff4e969cb75d82470cee29391ea75
Alex Quill <alexquill1@gmail.com>
2020-11-08 18:06:29 -0500

    bug fixes and minor polishing

--- src/components/UploadData.js
+++ src/components/UploadData.js
@@ -10,13 +10,16 @@ class UploadData extends Component {
 
     this.state = {
       appName: null,
+      appNameSubmitted: false,
       appCategory: null,
+      categorySubmitted: false,
       datasetName: null,
+      datasetNameSubmitted: false,
+      readyForButton: false,
       numAttributes: null,
       attributeNameList: [],
       attributeTypeList: [],
       numUsers: null,
-      // readyToGo: true,
     };
   }
 
@@ -45,9 +48,6 @@ class UploadData extends Component {
         console.log(data);
       }
     };
-
-    console.log('bingus');
-
     // Use this if we want apps to have categories as well.
     // let appCategory = null;
     // const categoryCallback = (data, error) => {
@@ -71,45 +71,137 @@ class UploadData extends Component {
     this.setState({ numUsers: null });
   }
 
-  addDatasetName = (event) => {
-    if (event.key === 'Enter') {
-      console.log('dataset name submitted');
-      this.setState({ datasetName: event.target.value });
-    }
-  }
-
-  addAppName = (event) => {
-    if (event.key === 'Enter') {
-      console.log('app name submitted');
-      this.setState({ appName: event.target.value });
-    }
-  }
-
   addNumUsers = (event) => {
-    if (event.key === 'Enter') {
+    console.log(typeof event.target.value);
+    if (!Number.isNaN(parseInt(event.target.value, 10))) {
       console.log('num users submitted');
       this.setState({ numUsers: event.target.value });
     }
   }
 
   addAppCategory = (event) => {
-    if (event.key === 'Enter') {
-      console.log('category submitted');
-      this.setState({ appCategory: event.target.value });
+    console.log('category submitted');
+    this.setState({ appCategory: event.target.value });
+    this.setState({ categorySubmitted: true });
+    if (this.state.datasetNameSubmitted && this.state.appNameSubmitted) {
+      this.setState({ readyForButton: true });
     }
   }
 
-  // startAgain = () => {
-  //   this.setState({ readyToGo: true });
-  //   this.setState({ datasetSubmitted: false });
-  // }
+  renderAppCategory = () => {
+    if (!this.state.categorySubmitted) {
+      return (
+        <div>
+          <h2>What category is your app in?</h2>
+          <div>
+            <div>
+              <select onChange={(e) => this.addAppCategory(e)}>
+                <option value="none">None</option>
+                <option value="health">Health</option>
+                <option value="location">Location</option>
+                <option value="consumer">Consumer</option>
+              </select>
+            </div>
+            <h4><i>invalid - app must have a category</i></h4>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>What category is your app in?</h2>
+          <div>
+            <div>
+              <select onChange={(e) => this.addAppCategory(e)}>
+                <option value="none">None</option>
+                <option value="health">Health</option>
+                <option value="location">Location</option>
+                <option value="consumer">Consumer</option>
+              </select>
+            </div>
+            <h4><i>Valid app category!</i></h4>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  addAppName = (event) => {
+    if (!(event.target.value === '')) {
+      console.log('app name submitted');
+      this.setState({ appName: event.target.value });
+      this.setState({ appNameSubmitted: true });
+    } else {
+      this.setState({ appNameSubmitted: false });
+    }
+  }
+
+  renderAppNameInput = () => {
+    if (!this.state.appNameSubmitted) {
+      return (
+        <div>
+          <h2>What is your app named?</h2>
+          <div>
+            <input id="appNameInput" type="text" placeholder="name" onChange={(e) => this.addAppName(e)} />
+            <h4><i>invalid - app must have a name</i></h4>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>What is your app named?</h2>
+          <div>
+            <input id="appNameInput" type="text" placeholder="name" onChange={(e) => this.addAppName(e)} />
+            <h4><i>Valid app name!</i></h4>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  addDatasetName = (event) => {
+    if (!(event.target.value === '')) {
+      console.log('dataset name submitted');
+      this.setState({ datasetName: event.target.value });
+      this.setState({ datasetNameSubmitted: true });
+    } else {
+      this.setState({ datasetNameSubmitted: false });
+    }
+  }
+
+  renderDatasetNameInput = () => {
+    if (!this.state.datasetNameSubmitted) {
+      return (
+        <div>
+          <h2>What is your dataset named?</h2>
+          <div>
+            <input id="nameInput" type="text" placeholder="name" onChange={(e) => this.addDatasetName(e)} />
+            <h4><i>invalid - dataset must have a name</i></h4>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>What is your dataset named?</h2>
+          <div>
+            <input id="nameInput" type="text" placeholder="name" onChange={(e) => this.addDatasetName(e)} />
+            <h4><i>Valid dataset name!</i></h4>
+          </div>
+        </div>
+      );
+    }
+  }
 
   renderAttributeFields = () => {
     if (!this.state.numAttributes) {
       return (
         <div>
+          <h2>Add Your Attributes</h2>
           <label htmlFor="attrNum">Number of Attributes:  </label>
-          <select id="attrNum" onChange={(e) => this.numAttributesOnChange(e)}>
+          <select key={Math.random()} id="attrNum" onChange={(e) => this.numAttributesOnChange(e)}>
+            <option value="1">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -117,7 +209,6 @@ class UploadData extends Component {
             <option value="5">5</option>
             <option value="6">6</option>
           </select>
-          {/* <input id="numAttributes" type="number" min="2" max="10" placeholder="5" value={this.state.value} onChange={(e) => this.numAttributesOnChange(e)} /> */}
         </div>
       );
     } else {
@@ -135,67 +226,51 @@ class UploadData extends Component {
           </div>,
         );
       }
-      return (
-        <div className="dataLists">
-          <div className="typesList">
-            {inputDatatypeFormList}
-            <button type="submit" onClick={() => { this.submitAttributes(); }}>Finish</button>
+      if (this.state.readyForButton) {
+        return (
+          <div className="dataLists">
+            <div className="typesList">
+              <h2>Add Your Attributes</h2>
+              {inputDatatypeFormList}
+              <button type="submit" onClick={() => { this.submitAttributes(); }}>Finish</button>
+            </div>
           </div>
-        </div>
-      );
+        );
+      } else {
+        return (
+          <div className="dataLists">
+            <div className="typesList">
+              <h2>Add Your Attributes</h2>
+              {inputDatatypeFormList}
+              <h3><i>Submit all forms to enter dataset</i></h3>
+            </div>
+          </div>
+        );
+      }
     }
   }
 
-  // renderSubmittedScreen = () => {
-  //   this.setState({ appName: null });
-  //   this.setState({ appCategory: null });
-  //   this.setState({ datasetName: null });
-  //   this.setState({ numAttributes: null });
-  //   this.setState({ attributeNameList: [] });
-  //   this.setState({ attributeTypeList: [] });
-  //   this.setState({ numUsers: null });
-
-  //   return (
-  //     <div>
-  //       <h1>Thank you for submitting your dataset</h1>
-  //       <button type="submit" onClick={this.startAgain()}> Start Again</button>
-  //     </div>
-  //   );
-  // }
+  renderNumUsersInput = () => {
+    return (
+      <div>
+        <h2>How many users does this app have?</h2>
+        <div>
+          <input id="numUsersInput" type="text" placeholder="name" onChange={(e) => this.addNumUsers(e)} />
+        </div>
+      </div>
+    );
+  }
 
   render() {
-    // if (this.state.datasetSubmitted) {
-    //   console.log('wowoweewow');
-    //   return (
-    //     <div>
-    //       {this.renderSubmittedScreen()}
-    //     </div>
-    //   );
-    // }
     return (
       <div>
         <h1>Upload Your Data</h1>
         <div>
-          <h2>What is your dataset named?</h2>
-          <div>
-            <input id="nameInput" type="text" placeholder="name" onKeyDown={(e) => this.addDatasetName(e)} />
-          </div>
-          <h2>What is your app named?</h2>
-          <div>
-            <input id="appNameInput" type="text" placeholder="name" onKeyDown={(e) => this.addAppName(e)} />
-          </div>
-          <h2>How many users does this app have?</h2>
-          <div>
-            <input id="numUsersInput" type="text" placeholder="name" onKeyDown={(e) => this.addNumUsers(e)} />
-          </div>
-          <h2>What category is your app in?</h2>
-          <div>
-            <input id="categoryInput" type="text" placeholder="name" onKeyDown={(e) => this.addAppCategory(e)} />
-          </div>
-          <h2>Add Your Attributes</h2>
-          <div>
-            {this.renderAttributeFields()}
-          </div>
+          {this.renderDatasetNameInput()}
+          {this.renderAppNameInput()}
+          {this.renderNumUsersInput()}
+          {this.renderAppCategory()}
+          {this.renderAttributeFields()}
         </div>
       </div>
     );
