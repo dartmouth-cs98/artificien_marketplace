75e728362093825174e607624050776a07d02092
tobiaslange18 <tobias.b.lange.21@dartmouth.edu>
2020-11-10 10:25:43 -0500

    rebasing gone wrong

--- src/components/DatasetSideNav.js
+++ src/components/DatasetSideNav.js
@@ -33,13 +33,33 @@ class DatasetSideNav extends Component {
             <div className="text-center">
               <h2>{this.props.content.dataset_id.S}</h2>
               <p>Pulled From: {this.props.content.app.S}</p>
-              <p># of Devices: {this.props.content.num_devices.N}</p>
               <p>Category: {this.props.content.category.S}</p>
-              <p>Predictable Attributes: {this.props.content.predictable_attributes.S}</p>
+              <p>Predictable Attributes: <i>None</i></p>
             </div>
             <div className="list-group" />
           </div>
         </div>
+      );
+    }
+  }
+
+  render() {
+    if (!this.props.content) {
+      return (
+        <div className="overlay" style={this.props.style}>
+          <button type="button" className="closebtn" onClick={this.props.onClick}>-</button>
+          <div className="sidenav-container">
+            <div className="text-center">
+              <h2>Dataset X</h2>
+              <p>Dataset details go here</p>
+            </div>
+            <div className="list-group" />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>{this.renderDatasetCard()}</div>
       );
     }
   }
