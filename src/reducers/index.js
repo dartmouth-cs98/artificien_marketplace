5d426a47bb5b17958ef4d04c401a9b26cf9dcc39
Alex Quill <alexquill1@gmail.com>
2021-01-14 09:32:50 -0500

    init redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,19 @@
 // the starting point for your redux store
 // this defines what your store state will look like
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 
-import CountReducer from './count-reducer';
+// import CountReducer from './count-reducer';
 
-const rootReducer = combineReducers({
-  count: CountReducer,
-});
+// const rootReducer = combineReducers({
+//   count: CountReducer,
+// });
+
+const initState = {
+  test: 'test',
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
 
 export default rootReducer;
