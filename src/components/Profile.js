b82c81c44a5d9789e9026adf9d39ede459a8a6a9
tobiaslange18 <tobias.b.lange.21@dartmouth.edu>
2021-01-19 22:09:27 -0500

    updated profile, working on upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -33,6 +33,7 @@ class Profile extends Component {
         console.log(data);
         const name = data.accessToken.payload.username;
         this.queryUser(name);
+        console.log(name);
       });
   }
 
@@ -49,6 +50,7 @@ class Profile extends Component {
       if (error) {
         console.log(error);
       } else {
+        console.log(data);
         console.log(data.Items[0]);
         this.setState({ userData: data.Items[0] });
       }
@@ -57,6 +59,7 @@ class Profile extends Component {
   }
 
   // -------------------------------------------------------- RENDER -------------------------------------------------------- //
+  // 2 errors rn: cannot print on new lines, struggling to print out the arrays for bank name and number
   render() {
     console.log(this.state.userData);
     if (this.state.userData) {
@@ -64,18 +67,28 @@ class Profile extends Component {
         <div className="profile-page-body">
           <div className="profile-page-user-info">
             <div id="change-profile-info">
-              <h3 id="profile-page-user-info-item">Name: {this.state.userData.username.S}</h3>
+              <h2 id="profile-page-user-info-item">Name: {this.state.userData.username.S}</h2>
               <button type="button" onClick={() => this.setState({ usernameChange: true })}>Change!</button>
               {this.renderChangeButton()}
             </div>
-            <h3 id="profile-page-user-info-item">Username: {this.state.userData.user_id.S}</h3>
-            <h3 id="profile-page-user-info-item">Email: {this.state.userData.user_account_email.S}</h3>
+            <h2 className="profile-page-user-info-item">Username: {this.state.userData.user_id.S}</h2>
+            <h2 className="profile-page-user-info-item">Email: {this.state.userData.user_account_email.S}</h2>
+            <h2 className="profile-page-user-info-item">Username: {this.state.userData.user_id.S}</h2>
+            <h2 className="profile-page-user-info-item">Enterprise: {this.state.userData.enterprise.S}</h2>
+            <h2 className="profile-page-user-info-item">Date Joined: {this.state.userData.date_joined.S}</h2>
+            <h2 className="profile-page-user-info-item">Bank Name: {this.state.userData.bank_info.bank}</h2>
+            <h2 className="profile-page-user-info-item">Bank Number: {this.state.userData.bank_info.bank_number}</h2>
+          </div>
+          <div className="model-info">
+            <h2 className="profile-page-user-info-item">Models Submitted: {this.state.userData.num_models_submitted.N}</h2>
+            <h2 className="profile-page-user-info-item">Devices Reached: {this.state.userData.devices_reached.N}</h2>
+            <h2 className="profile-page-user-info-item">Average Training Time: {this.state.userData.average_training_days.N} days</h2>
           </div>
         </div>
       );
     } else {
       return (
-        <div>nothing yet</div>
+        <div>Nothing to see here!</div>
       );
     }
   }
