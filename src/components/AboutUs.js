d2e5764edb50311978b67946216b2b10fe6464f8
tobiaslange18 <tobias.b.lange.21@dartmouth.edu>
2021-02-22 15:51:20 -0500

    adding headshots / text for about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -20,8 +20,18 @@ class AboutUs extends Component {
       <>
         <div className="body">
           <h1>Vision</h1>
-          <a href="https://jupyter.artificien.com/hub/login" title="embedded jupyter" target="_blank" rel="noreferrer">Click here to upload your model</a>
+          <p>to be filled in.</p>
           <h1>Team</h1>
+          <img src="MK_headshot.jpg" alt="matt" />
+          <p>text about mr. matt</p>
+          <img alt="matt" src="img/JE_headshot.jpg" />
+          <p>text about mr. jake</p>
+          <img alt="matt" src="img/AQ_Headshot.jpg" />
+          <p>text about mr. quill</p>
+          <img alt="matt" src="img/TL_headshot.jpg" />
+          <p>text about mr. bobe</p>
+          <img alt="matt" src="img/SA_headshot.jpg" />
+          <p>text about mr. shrey</p>
           <div className="footer"> <BottomNav /> </div>
         </div>
       </>
