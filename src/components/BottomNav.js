799c8d1b2610d50288ae12a8da3c22ffa03b68ca
tobiaslange18 <tobias.b.lange.21@dartmouth.edu>
2021-02-24 23:53:54 -0500

    added to bottom nav

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -26,10 +26,11 @@ class BottomNav extends Component {
           <p><NavLink ClassName="active" to="/marketplace"> Marketplace </NavLink></p>
           <p><NavLink to="/models"> My Models </NavLink></p>
           <p><NavLink to="/upload_data"> Data Upload </NavLink></p>
+          <p><NavLink to="/how_it_works"> How It Works </NavLink></p>
         </div>
         <div id="bottom-nav-column">Help
-          <p><a href="https://artificien.zendesk.com/hc/en-us">Support Desk</a></p>
           <p><NavLink to="/documentation"> Documentation </NavLink></p>
+          <p><a href="https://artificien.zendesk.com/hc/en-us">Support Desk</a></p>
         </div>
         <div id="bottom-nav-column">Contact
           <p> <a href="https://www.linkedin.com/in/tobias-lange/" className="fa fa-linkedin" /> </p>
