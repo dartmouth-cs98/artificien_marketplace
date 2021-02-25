c319ba514422f31124385b7d1ef3f5aeaddf4f0a
tobiaslange18 <tobias.b.lange.21@dartmouth.edu>
2021-02-24 23:58:20 -0500

    updated content per shreyas' request

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -17,11 +17,11 @@ class HowItWorks extends Component {
     return (
       <div className="body">
         <h1>How Artificien Works: Data Scientists</h1>
-        <p>1. They select the dataset they want to purchase from the <i><b>Artificien Marketplace</b></i></p>
-        <p>2. They create a new machine learning model using PyTorch in the <i><b>Artificien JupyterHub</b></i></p>
-        <p>3. They utilize the <i><b>Artificien Library</b></i> functions that facilitate federated learning smoothly, sending their model directly to devices</p>
-        <p>4. They track their progress in the <i><b>Artificien Models</b></i> tab</p>
-        <p>5. They download their trained model</p>
+        <p>1. Select the dataset they want to purchase from the <i><b>Artificien Marketplace</b></i></p>
+        <p>2. Create a new machine learning model using PyTorch in the <i><b>Artificien JupyterHub</b></i></p>
+        <p>3. Utilize the <i><b>Artificien Library</b></i> functions that facilitate federated learning smoothly, sending their model directly to devices</p>
+        <p>4. Track their progress in the <i><b>Artificien Models</b></i> tab</p>
+        <p>5. Download their trained model</p>
       </div>
     );
   }
@@ -30,9 +30,10 @@ class HowItWorks extends Component {
     return (
       <div className="body">
         <h1>How Artificien Works: App Developers</h1>
-        <p>1. They fill out the <i><b>Artificien Upload</b></i> form, signaling that they have a new dataset to sell access to</p>
-        <p>2. They download the <i><b>Artificien Cocoapod</b></i>, connecting their application to <i><b>Artificien Infrastructure</b></i></p>
-        <p>3. They profit! Making money whenever a data scientist purchases access to their dataset</p>
+        <p>1. Fill out the <i><b>Artificien Upload</b></i> form, signaling that they have a new dataset to sell access to</p>
+        <p>2. Integrate the <i><b>Artificien Cocoapod</b></i> with your native iOS app</p>
+        <p>3. Pre-process and expose your data to <i><b>Artificien Infrastructure</b></i> for on-device training</p>
+        <p>4. Profit! Making money whenever a data scientist purchases access to their dataset</p>
       </div>
     );
   }
